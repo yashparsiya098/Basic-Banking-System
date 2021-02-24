41f6a17b7885c6661431efdb0c334c3fd62d8b20
yashparsiya098 <71448850+yashparsiya098@users.noreply.github.com>
2021-02-24 22:10:51 +0530

    Add files via upload

---
+++ banking system/config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$conn = mysqli_connect('localhost','id16237846_yash','xi2a/b/5og\u(t2I','id16237846_yashcs');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
